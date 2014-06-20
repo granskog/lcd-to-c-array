c414280ff53f30a32f17519748035dffe60a6452
granskog <granskog@users.noreply.github.com>
2014-06-20 12:37:50 +0200

    Updated the contact info.
    Updated the contact information with github web address.

--- lcdtocarray.py
+++ lcdtocarray.py
@@ -8,7 +8,7 @@
 
 # The MIT License (MIT)
 
-# Copyright (c) 2014 David Gran Skog, http://www.granskog.com
+# Copyright (c) 2014 David Gran Skog, http://www.granskog.com, https://github.com/granskog/lcd-to-c-array
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
